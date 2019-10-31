30428060b92413e911369fa6f5c38a0627a67017
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-30 23:36:02 -0600

    Remove check for emailed trips

--- app.rb
+++ app.rb
@@ -6,20 +6,17 @@ require './app/controllers/fetcher'
 require './app/controllers/mailer'
 require './app/models/emailed_trip'
 require 'rufus-scheduler'
-require 'net/http'
 
 class App < Sinatra::Base
   scheduler = Rufus::Scheduler.new
-  scheduler.every '30s' do
-    fetcher = Fetcher.new
-    # ActiveRecord::Base.connection_pool.with_connection do
-      fetcher.get_trips.each do |trip|
-        unless EmailedTrip.exists?(trip_id: trip[:id])
-          print "Emailing contacts for #{trip[:name]}"
-          EmailedTrip.create(trip_id: trip[:id])
-          Mailer.notify(trip).deliver_now
-        end
-      end
-    # end
+  fetcher = Fetcher.new
+  scheduler.every '10s' do
+    fetcher.get_trips.each do |trip|
+      # unless EmailedTrip.exists?(trip_id: trip[:id])
+        print "Emailing contacts for #{trip[:name]}"
+        # EmailedTrip.create(trip_id: trip[:id])
+        Mailer.notify(trip).deliver_now
+      # end
+    end
   end
 end
