34d4f51a6b50a89690efd0f8a7cc7b1f9d287db7
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-10-30 23:43:55 -0600

    Add emailed_trip check back

--- app.rb
+++ app.rb
@@ -6,17 +6,22 @@ require './app/controllers/fetcher'
 require './app/controllers/mailer'
 require './app/models/emailed_trip'
 require 'rufus-scheduler'
+require 'net/http'
 
 class App < Sinatra::Base
   scheduler = Rufus::Scheduler.new
-  fetcher = Fetcher.new
-  scheduler.every '10s' do
-    fetcher.get_trips.each do |trip|
-      # unless EmailedTrip.exists?(trip_id: trip[:id])
-        print "Emailing contacts for #{trip[:name]}"
-        # EmailedTrip.create(trip_id: trip[:id])
-        Mailer.notify(trip).deliver_now
-      # end
+  # ActiveRecord::Base.establish_connection
+  scheduler.every '30s' do
+    fetcher = Fetcher.new
+    ActiveRecord::Base.connection_pool.with_connection do
+      fetcher.get_trips.each do |trip|
+        unless EmailedTrip.exists?(trip_id: trip[:id])
+          binding.pry
+          print "Emailing contacts for #{trip[:name]}"
+          EmailedTrip.create(trip_id: trip[:id])
+          Mailer.notify(trip).deliver_now
+        end
+      end
     end
   end
 end
